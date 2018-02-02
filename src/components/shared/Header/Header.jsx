6619c824e50615a85062c256fe51b92f3bade05f
wwwhatley <30512540+wwwhatley@users.noreply.github.com>
2018-02-01 20:01:51 -0500

    Made styling changes to header.

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -9,11 +9,13 @@ const user = {
   avatar: ""
 };
 
-const imgStyle = {
-  width: "200px",
-  height: "41px",
-  margin: "0 1.5em"
-};
+const Img = styled.img`
+  width: 200px;
+  height: 41px;
+  margin: 0 1.5em;
+`;
+
+const imgStyle = {};
 
 const Wrapper = styled.section`
   height: 75px;
@@ -27,24 +29,24 @@ const Wrapper = styled.section`
   top: 0;
 `;
 
-const Header = ({ children, props }) => (
-  <Wrapper>
-    <nav>
-      <Row alignitems="center">
-        <img src={Logo} style={imgStyle} alt="Subchannel Logo" />
-        <NavLink to="/">
-          <Title3 fontSize=".9em" fontWeight="300">
-            Industries
-          </Title3>
-        </NavLink>
-      </Row>
-    </nav>
-    <div style={{ marginRight: "1.5em" }}>
-      <Title3 fontSize="1em" fontWeight="700">
-        Log In
-      </Title3>
-    </div>
-  </Wrapper>
-);
-
-export default Header;
+export default function Header({ children, ...props }) {
+  return (
+    <Wrapper>
+      <nav>
+        <Row alignitems="center">
+          <Img src={Logo} alt="Subchannel Logo" />
+          <NavLink to="/">
+            <Title3 fontSize=".9em" fontWeight="300">
+              Industries
+            </Title3>
+          </NavLink>
+        </Row>
+      </nav>
+      <div style={{ marginRight: "1.5em" }}>
+        <Title3 fontSize="1em" fontWeight="700">
+          Log In
+        </Title3>
+      </div>
+    </Wrapper>
+  );
+}
